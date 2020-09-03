55e2afa00ee910cb886b5f886872edd1b51b9723
Nicolas Schmidt <nschmidtg@gmail.com>
2020-09-02 21:48:02 -0400

    initial commit

---
+++ app/javascript/components/post-grid.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h3>
+      Portfolio
+    </h3>
+    <div
+      v-for="post in posts"
+      :key="post.key"
+    >
+      {{ post.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { getPosts } from '../api/post';
+
+export default {
+  
+  data() {
+    return {
+      posts: {
+        type: Object,
+        default: [],
+      }
+    };
+  },
+  methods: {
+  },
+  mounted() {
+    if (!this.initialLoad) {
+      getPosts()
+        .then((response) => {
+          this.posts = response.data.data
+          console.log(response)
+        });
+      console.log(this.posts)
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
