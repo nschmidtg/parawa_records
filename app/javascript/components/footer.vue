75513e1b02a21c215916bbdb82604fcb85cd1fad
Nicolas Schmidt <nschmidtg@gmail.com>
2020-09-05 12:48:17 -0400

    feat(design): added styles

---
+++ app/javascript/components/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer">
+    <h2>Nicolás Schmidt</h2>
+    <p>
+      Todos los derechos reservados - 2020
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
---
+++ app/javascript/components/header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="crop">
+      <img v-bind:src="studioImage">
+      <div class="text-centered-image">
+        <h1>NICOLÁS SCHMIDT GUBBINS</h1>
+        <h3>PRODUCCIÓN MUSICAL</h3>
+      </div>
+    </div>
+
+    <div id="navbar">
+      <div class="grouped-buttons">
+        <a class="active" href="javascript:void(0)">Sobre mí</a>
+        <a href="javascript:void(0)">Portafolio</a>
+        <a href="javascript:void(0)">Contacto</a>
+      </div>  
+    </div>
+  </div>  
+</template>
+
+<script>
+
+var navbar, sticky;
+import studioImage from 'images/studioCropped.png';
+export default {
+   data() {
+    return {
+      studioImage,
+    }
+  },
+  methods: {
+    addSticky() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+  },
+  mounted() {
+    navbar = document.getElementById("navbar");
+    sticky = 300;
+    console.log(sticky)
+    window.addEventListener('scroll', this.addSticky);
+  },
+}
+</script>
